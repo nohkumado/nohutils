38f77876b09318f13e1b54293e88f21f917ceaaa
NohKumaDo <nohkumado@gmail.com>
2017-01-02 09:28:11 +0100

    added indexed get for treemap, also a toString methog to dump the treemap

--- nohutils/src/main/java/com/nohkumado/nohutils/collection/TreeMapTable.java
+++ nohutils/src/main/java/com/nohkumado/nohutils/collection/TreeMapTable.java
@@ -1,29 +1,96 @@
 package com.nohkumado.nohutils.collection;
-import java.util.*;
 import android.util.*;
+import java.util.*;
 
-public class TreeMapTable<E,G>
+public class TreeMapTable<E,G> implements Iterable
 {
   TreeMap<E,ArrayMap<E,G>> rows = new TreeMap<>();
-  
+  TreeMap<Integer,E> colNames =   new TreeMap<>();
+
   public G set(E row, E col, G val)
   {
     ArrayMap<E,G> aRow = rows.get(row);
-    if(aRow == null)
+    if (aRow == null)
     {
       aRow = new ArrayMap<>();
-      rows.put(row,aRow);
+      rows.put(row, aRow);
     }
-    aRow.put(col,val);
+    aRow.put(col, val);
+    if (!colNames.containsValue(col)) colNames.put(colNames.size(), col);
     return val;
   }
   public G get(E row, E col)
   {
-    ArrayMap<E,G> aRow = rows.get(row);
-    if(aRow != null)
+    try
     {
+      ArrayMap<E,G> aRow = rows.get(row);
       return aRow.get(col);
+    }
+    catch (NullPointerException e)
+    {
+
     }
     return null;
   }
+  public E get(int row)
+  {
+    int i= 0;
+    E theKey = null;
+    for (E aKey:  rows.keySet())
+    {
+      if (i == row)
+      {
+        theKey = aKey;
+        break;
+      }
+      i++;
+    }
+    return theKey;
+  }
+  public int size()
+  {
+    return rows.size();
+  }
+
+  @Override
+  public String toString()
+  {
+    StringBuilder sb = new StringBuilder();
+    sb.append("%10s ");
+    for (int i = 0; i <= colNames.size(); i++) sb.append("%10s ");
+    String form = sb.toString();
+    sb =  new StringBuilder();
+    sb.append(String.format("%3s %10s ", "num", "token"));
+    for (Map.Entry<Integer,E> col: colNames.entrySet())
+    {
+      sb.append(String.format("%10s ", col.getValue()));
+    }
+    sb.append("\n");
+    int i = 0; 
+    for (Map.Entry<E,ArrayMap<E,G>> row: rows.entrySet())
+    {
+      sb.append(String.format("%3d %-10s ", i, row.getKey()));
+      i++;
+
+      ArrayMap<E,G> aRow = row.getValue();
+      for (Map.Entry<Integer,E> col: colNames.entrySet())
+      {
+        E lkey = col.getValue();
+        if (aRow.containsKey(lkey)) sb.append(String.format("%-10s ", aRow.get(lkey)));
+        else sb.append(String.format("%10s ", " "));
+      }
+      sb.append("\n");
+    }
+
+
+    return sb.toString();
+  }
+
+  @Override
+  public Iterator iterator()
+  {
+    return rows.keySet().iterator();
+  }
+
+  
 }
