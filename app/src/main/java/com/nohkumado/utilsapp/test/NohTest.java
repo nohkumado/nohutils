259a1018807f2ea567a2c3ee8a8b17d51dc342b2
Bruno Boettcher <nokumado@gmail.com>
2017-12-14 22:07:15 +0100

    added junit compatible assert methods to the UtilsTester, added test to test the UtilsTester

--- app/src/main/java/com/nohkumado/utilsapp/test/NohTest.java
+++ app/src/main/java/com/nohkumado/utilsapp/test/NohTest.java
@@ -1,71 +1,79 @@
 package com.nohkumado.utilsapp.test;
+
 import com.nohkumado.nohutils.ShellI;
-import com.nohkumado.nohutils.test.*;
+import com.nohkumado.nohutils.test.BTreeAtomTest;
+import com.nohkumado.nohutils.test.NTreeNodeTest;
+import com.nohkumado.nohutils.test.SortedArrayTest;
+import com.nohkumado.nohutils.test.TreeMapTableTest;
+import com.nohkumado.nohutils.test.UtilsTester;
+import com.nohkumado.nohutils.test.UtilsTesterTest;
 
 /**
- Begin, don't know where the proper testsuite fell of... :( somehow it disappeared...
- will slowly rebuild it here...
+ * Begin, don't know where the proper testsuite fell of... :( somehow it disappeared...
+ * will slowly rebuild it here...
  */
 @SuppressWarnings("UnusedParameters")
-public class NohTest extends UtilsTester
-{
-	private final static String TAG ="NoT";
+public class NohTest extends UtilsTester {
+  private final static String TAG = "NoT";
+  StringBuilder result = new StringBuilder();
+  UtilsTester tester = new TreeMapTableTest();
 
 
-	public NohTest(ShellI s)
-	{
-	}
+  public NohTest(ShellI s) {
+  }
 
-	@SuppressWarnings("UnusedAssignment")
-	public String runTest()
-	{
-		StringBuilder result = new StringBuilder();
-		UtilsTester tester = new TreeMapTableTest();
+  @SuppressWarnings("UnusedAssignment")
+  public String runTest() {
 
-		try
-		{
+    try {
 
+      test();
+    }//try
+    catch (NullPointerException e) {
+      result.append(tester.log()).append("\n").append(e.toString());
+    }
+    return result.toString();
+  }//public String runTest()
 
-			result.append("TreeMapTable test:");
-			if (tester.test()) result.append("success!");
-			else result.append("failed ! ");
-			//print("End TreeMapTable test");
+  @Override
+  public boolean test() {
+    result.append("UtilsTester test:");
+    tester = new UtilsTesterTest();
+    if (tester.test()) result.append("success!\n");
+    else result.append("failed ! \n").append(tester.log()).append("\n");
 
-			result.append("\nBTreeAtom test:");
-			tester = new BTreeAtomTest();
-			if (tester.test()) result.append("success!");
-			else 
-			{
-				result.append("failed ! ");
-				result.append(tester.log());
-			}
+    result.append("TreeMapTable test:");
+    tester = new TreeMapTableTest();
+    if (tester.test()) result.append("success!\n");
+    else result.append("failed ! ").append(tester.log()).append("\n");
+    //print("End TreeMapTable test");
 
-			//print("TreeMapTable test");
-			result.append("\nNTreeNode test:");
-			tester = new NTreeNodeTest();
-			if (tester.test()) result.append("success!");
-			else 
-			{
-				result.append("failed ! ");
-				result.append(tester.log());
-			}
+    result.append("BTreeAtom test:");
+    tester = new BTreeAtomTest();
+    if (tester.test()) result.append("success!");
+    else {
+      result.append("failed ! ");
+      result.append(tester.log());
+    }
 
-			result.append("\nSorted Array test:");
+    //print("TreeMapTable test");
+    result.append("\nNTreeNode test:");
+    tester = new NTreeNodeTest();
+    if (tester.test()) result.append("success!");
+    else {
+      result.append("failed ! ");
+      result.append(tester.log());
+    }
 
-			tester = new SortedArrayTest();
-			if (tester.test()) result.append("success!");
-			else 
-			{
-				result.append("failed ! ");
-				result.append(tester.log());
-			}
-		}//try
-		catch (NullPointerException e)
-		{
-			result.append(tester.log()).append("\n").append(e.toString());
-		}
-		return result.toString();
-	}//public String runTest()
+    result.append("\nSorted Array test:");
 
+    tester = new SortedArrayTest();
+    if (tester.test()) result.append("success!");
+    else {
+      result.append("failed ! ");
+      result.append(tester.log());
+    }
+    return true;
+  }
 }//public class NohTest extends UtilsTester
 
--- nohutils/src/main/java/com/nohkumado/nohutils/test/UtilsTester.java
+++ nohutils/src/main/java/com/nohkumado/nohutils/test/UtilsTester.java
@@ -3,116 +3,148 @@
  */
 package com.nohkumado.nohutils.test;
 
-import com.nohkumado.nohutils.*;
+import android.util.Log;
 
-import android.util.*;
+import com.nohkumado.nohutils.ReturnValue;
 
 /**
  * @author bboett
- * 
  */
 @SuppressWarnings({"WeakerAccess", "CanBeFinal"})
-public class UtilsTester
-{
-  private final static String TAG ="UT";
+public abstract class UtilsTester {
+  private final static String TAG = "UT";
   protected StringBuilder log = new StringBuilder();
   protected boolean immediate = false;
-	protected StringBuilder error;
-	protected static String testName = "UtilsTester";
+  protected StringBuilder error;
+  protected static String testName = "UtilsTester";
 
-	public String log()
-	{
-		if(log != null) return log.toString();
-		return "";
-	}
+  public String log() {
+    if (log != null) return log.toString();
+    return "";
+  }
 
-	public boolean test()
-	{
-		// TODO: Implement this method
-		return false;
-	}
-  /** 
-   * 
-   * 
+  abstract public boolean test() ;
+
+  /**
    * @param result result
    */
-  public boolean doTrans(boolean result, String msg)
-  {
-    if (result)
-    {
-      if (msg == null || msg.equals(""))
-      {
+  public boolean doTrans(boolean result, String msg) {
+    if (result) {
+      if (msg == null || msg.equals("")) {
         error(testName);
       }// if(msg != null || !msg.equals(""))
-      else
-      {
+      else {
         error(testName + " " + msg);
       }// else
     }//# if(toCheck.status())
-    return(!result);
+    return (!result);
   }// public void doTrans(Item toCheck)
-  /** 
-   * 
-   * 
+
+  /**
    * @param result result
    */
-  public boolean doTrans(boolean result, StringBuilder msg)
-  {
-    if (result)
-    {
-      if (msg == null || msg.length() <= 0)
-      {
+  public boolean doTrans(boolean result, StringBuilder msg) {
+    if (result) {
+      if (msg == null || msg.length() <= 0) {
         error(testName);
       }// if(msg != null || !msg.equals(""))
-      else
-      {
+      else {
         error(testName + " " + msg);
       }// else
     }//# if(toCheck.status())
-    return(!result);
+    return (!result);
   }// public void doTrans(Item toCheck)
-  
-  /** 
-   * 
-   * 
+
+  /**
    * @param status status
    */
-  public boolean doTrans(ReturnValue status)
-  {
+  public boolean doTrans(ReturnValue status) {
     return doTrans(!status.status(), status.report());
   }// public void doTrans(Item toCheck)
-  /** 
-   * print 
-   * 
-   * @param m  msg
+
+  /**
+   * print
+   *
+   * @param m msg
    */
-  public void print(String m)
-  {
-	  Log.d(TAG,"print appendung to "+log+" m");
+  public void print(String m) {
+    Log.d(TAG, "print appendung to " + log + " m");
     if (immediate) Log.d(TAG, m);
     else log.append(m).append("\n");
   }// public void print(String m)
-  /** 
-   * error 
-   * 
+
+  /**
+   * error
+   *
    * @param m msg
    */
-  public void error(String m)
-  {
-	  Log.d(TAG,"err appending to "+log+" "+m);
+  public void error(String m) {
+    Log.d(TAG, "err appending to " + log + " " + m);
     if (immediate) Log.e(TAG, m);
     else log.append("ERROR:").append(m).append("\n");
   }// public void print(String m)
-  /** 
-   * error 
-   * 
+
+  /**
+   * error
+   *
    * @param m msg
    */
-  public StringBuilder error(StringBuilder m)
-  {
+  public StringBuilder error(StringBuilder m) {
     if (immediate) Log.e(TAG, m.toString());
     else log.append("ERROR:").append(m).append("\n");
     return m;
   }// public void print(String m)
-  
+
+  public boolean assertTrue(boolean cond, String msg) {
+    if (cond) return cond;
+    error(msg);
+    return cond;
+  }
+
+  public boolean assertFalse(boolean cond, String msg) {
+    return assertTrue(!cond, msg);
+  }
+  public boolean assertTrue(boolean cond) {
+    if (cond) return cond;
+    return cond;
+  }
+
+  public boolean assertFalse(boolean cond) {
+    return assertTrue(!cond);
+  }
+  public boolean assertNull(Object cond, String msg) {
+    return assertTrue((cond == null), msg);
+  }
+
+  public boolean assertNotNull(Object cond, String msg) {
+    return assertTrue((cond != null), msg);
+  }
+  public boolean assertNull(Object cond) {
+    return assertTrue(cond == null);
+  }
+
+  public boolean assertNotNull(Object cond) {
+    return assertTrue(cond != null);
+  }
+
+  public boolean assertEquals(Object one, Object two) {
+    if (assertNull(one) && assertNull(two)) return true;
+    if (assertNull(one) || assertNull(two)) return false;
+    return assertTrue((one.equals(two)));
+  }
+
+  public boolean assertNotEquals(Object one, Object two)
+  {
+    return !assertEquals(one,two);
+  }
+
+  public boolean assertArrayEquals(Object[] one, Object[] two) {
+    if (assertNull(one) && assertNull(two)) return true;
+    if (assertNull(one) || assertNull(two)) return false;
+    if (assertFalse(one.length == two.length)) return false;
+    for (int i = 0; i < one.length; i++)
+      if (assertNotEquals(one[i], two[i])) return false;
+    return true;
+
+  }
 }// public class UtilsTester
